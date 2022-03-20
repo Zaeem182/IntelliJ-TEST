aac8abf41e2f467ea7d51aabb1fec41bc08f702d
Ali <alimohamedreddit@gmail.com>
2022-03-20 18:04:32 +0200

    Ali added pc class

---
+++ src/main/java/PC.java
@@ -0,0 +1,33 @@
+public class PC {
+    private String name, id, size;
+
+    public PC(String name, String id, String size) {
+        this.name = name;
+        this.id = id;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
